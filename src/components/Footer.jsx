b7105d1401e7da9dcb3bf453a214a75265669004
TejasChoukale <144011993+TejasChoukale@users.noreply.github.com>
2025-04-27 10:49:14 +0530

    your message about the change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,18 +8,18 @@ function Footer() {
     <footer>
       <div className="developer-info">
         <img 
-          src="/Tejas Profile.jpg"  
-          alt="Tejas Choukale" 
+          src="/ajaypawar.jpg"  
+          alt="Ajay Pawar" 
           className="developer-photo"
         />
         <div style={{ marginLeft: '20px' }}>
-          <h3>Developed by Tejas Shankar Choukale</h3>
+          <h3>Developed by Ajay Pawar</h3>
         </div>
       </div>
       <div className="social-links">
-        <a href="https://github.com/TejasChoukale/" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-        <a href="https://www.instagram.com/tejaschoukale/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-        <a href="tejasshankarchoukale@gmail.com"><FaEnvelope /></a>
+        <a href="https://github.com/dashboard" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+        <a href="https://www.instagram.com/mr_ajay_6711?igsh=MXc3ZDVlZ3BvZGx1ZQ==" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+        <a href="pawarajaylaxman123@gmail.com"><FaEnvelope /></a>
       </div>
       <p>Copyright © {year} Keeper App. All Rights Reserved.</p>
     </footer>
